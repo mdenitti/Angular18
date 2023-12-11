ad976653bb1d4dceb9153a8ff7d9d5aa4b333811
mdenitti <massimodn77@gmail.com>
2023-12-11 13:46:52 +0100

    Added json pipe operator to debug and see what is in array without iterating it... also added uppercase pipe to content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,7 +16,15 @@ export class HomeComponent {
   title = 'xMas Shopping';
   myContent:string = '';
   url:string = 'http://localhost:3000/presents';
+  urlUsers:string = 'http://localhost:3000/users';
+
+  // my todos should also be an array of objects - using any as type for now
+  // obviously we should use a model or an interface
   todos:any[] = [];
+  // my users should also be an array of objects
+  users:any[] = [];
+
+  // single instance of user i aquire using ngmodel on the select in my view
   user: any;
 
   increaseCount() {
@@ -29,7 +38,6 @@ export class HomeComponent {
     }
 
   postData() {
-
     const options = {
       method: 'POST',
       headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0'},
@@ -56,9 +64,16 @@ export class HomeComponent {
       .then(response => response.json())
       .then(json => this.todos = json)
   }
+
+  fetchMyUsers() {
+    fetch(this.urlUsers)
+      .then(response => response.json())
+      .then(json => this.users = json)
+  }
   
   ngOnInit() {
     this.fetchMyData();
+    this.fetchMyUsers();
    // Retrieve count from local storage when the component initializes
    const storedCount = localStorage.getItem('count');
    this.count = storedCount !== null ? parseInt(storedCount) : 0;
