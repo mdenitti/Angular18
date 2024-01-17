aa00e34282830d5f378e31efa6f510f83fef6fba
mdenitti <massimodn77@gmail.com>
2024-01-17 15:33:29 +0100

    added dynamic AND easy multiple input fields to the about component and view

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UsersService } from '../shared/users.service';
 import { MyUsers } from '../my-users.interface';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
@@ -14,14 +15,32 @@ export class AboutComponent {
 
   constructor(private usersService: UsersService) { }
 
-users: MyUsers[] = []  
-apiurl = this.usersService.url
+  users: MyUsers[] = []
+  apiurl = this.usersService.url
 
-ngOnInit() {
-  this.usersService.getUsers().then(data => {
-    this.users = data
-  }).catch(error => console.log(error)
-  )
-}
+  fieldsArray = ['', '']; // Initialize with two fields... it's up to you to add more
+
+  addField() {
+    // Add a new field
+    this.fieldsArray.push('');
+  }
+
+  submitForm() {
+    // making use of the destructor again ;) yay
+    const values = [...this.fieldsArray];
+    console.log(values);
+
+    // clear the fields;
+    this.fieldsArray = ['', ''];
+
+    alert(values.join('\n'));
+  }
+
+  ngOnInit() {
+    this.usersService.getUsers().then(data => {
+      this.users = data
+    }).catch(error => console.log(error)
+    )
+  }
 
 }
